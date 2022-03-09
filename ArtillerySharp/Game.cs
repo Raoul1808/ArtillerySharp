ac6f63b4f8f650c50b4c603f8404b185c4a24b69
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2022-03-09 18:06:18 +0100

    Create Game Window

---
+++ ArtillerySharp/Game.cs
@@ -0,0 +1,12 @@
+﻿using OpenTK.Windowing.Desktop;
+
+namespace ArtillerySharp
+{
+    public class Game : GameWindow
+    {
+        public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+            : base(gameWindowSettings, nativeWindowSettings)
+        {
+        }
+    }
+}
--- ArtillerySharp/Program.cs
+++ ArtillerySharp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
 
 namespace ArtillerySharp
 {
@@ -6,7 +9,26 @@ namespace ArtillerySharp
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            GameWindowSettings gameWindowSettings = new GameWindowSettings()
+            {
+                IsMultiThreaded = false,
+                RenderFrequency = 60,
+                UpdateFrequency = 60,
+            };
+            NativeWindowSettings nativeWindowSettings = new NativeWindowSettings()
+            {
+                API = ContextAPI.OpenGL,
+                APIVersion = new Version(3, 3),
+                IsFullscreen = false,
+                Title = "Artillery#",
+                WindowBorder = WindowBorder.Fixed,
+                Size = new Vector2i(1280, 720),
+            };
+
+            using (Game game = new Game(gameWindowSettings, nativeWindowSettings))
+            {
+                game.Run();
+            }
         }
     }
 }
